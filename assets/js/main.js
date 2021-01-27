f79abbd31ef113bf248ade02781bfe67f5ea404c
houra49 <houra.shamshyrcar@gmail.com>
2021-01-28 00:26:37 +0100

    Lev3_4_Project_css-vertiefung_design-interior

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+function showMenu() {
+    console.log(document.getElementById("name").value)
+    console.log(document.getElementById("email").value)
+    console.log(document.getElementById("message").value)
+}
